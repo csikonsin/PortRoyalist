83e16903c3db1f56adf1f61f73a768076d64127e
Daniel <daniel.giczi@edu.fh-joanneum.at>
2022-11-12 21:12:54 +0100

    Test

---
+++ PortRoyalist/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PortRoyalist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScreenShotParser.Init();
+            var ssp = new ScreenShotParser();
+
+            ssp.PrepareScreenshots();
+
+            var path = ssp.ParseScreenshots();
+
+            if(path != null)
+            {
+                var winFormImg = System.Drawing.Image.FromFile(path.FullName);
+                img.Source = ImageUtils.ToImageSource(winFormImg, ImageFormat.Jpeg);
+                img.Width = winFormImg.Width;
+                img.Height= winFormImg.Height;
+            }
+            else{
+                this.Close();
+            }
+
+
+        }
+    }
+
+
+    public class ImageUtils
+    {
+        public static ImageSource ToImageSource(System.Drawing.Image image, ImageFormat imageFormat)
+        {
+            BitmapImage bitmap = new BitmapImage();
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                // Save to the stream
+                image.Save(stream, imageFormat);
+
+                // Rewind the stream
+                stream.Seek(0, SeekOrigin.Begin);
+
+                // Tell the WPF BitmapImage to use this stream
+                bitmap.BeginInit();
+                bitmap.StreamSource = stream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+            }
+
+            return bitmap;
+        }
+    }
+}
